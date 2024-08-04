517b386016076ab39616f88ca29a6d7ab7c121b5
Radoslaw Kamil Ejsmont <radoslaw@ejsmont.net>
2024-08-04 22:43:23 +0200

    Something changed

--- libzfseasy/__init__.py
+++ libzfseasy/__init__.py
@@ -1,39 +1,40 @@
 from libzfseasy.zfs import List, Create, Snapshot, Bookmark, Destroy, Rename, Allow, UnAllow, Clone, Get, Set, \
      Inherit, Send, Receive, ChangeKey, LoadKey, UnLoadKey
 
-allow = Allow()
-bookmark = Bookmark()
-change_key = ChangeKey()
-clone = Clone()
+list = List()
 create = Create()
+snapshot = Snapshot()
+bookmark = Bookmark()
 destroy = Destroy()
-# diff
+rename = Rename()
+allow = Allow()
+unallow = UnAllow()
+clone = Clone()
 get = Get()
+set = Set()
+inherit = Inherit()
+send = Send()
+receive = Receive()
+recv = Receive()
+load_key = LoadKey()
+unload_key = UnLoadKey()
+change_key = ChangeKey()
+# mount
+# unmount
+
+# diff
 # groupspace
 # hold
-inherit = Inherit()
 # jail
-list = List()
-load_key = LoadKey()
-# mount
 # program
 # project
 # projectspace
 # promote
-receive = Receive()
-recv = Receive()
 # redact
 # release
-rename = Rename()
 # rollback
-send = Send()
-set = Set()
 # share
-snapshot = Snapshot()
-unallow = UnAllow()
 # unjail
-unload_key = UnLoadKey()
-# unmount
 # upgrade
 # userspace
 # wait
